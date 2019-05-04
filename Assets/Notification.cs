8f66b6b9cda2f5d69471efd53ee67160873427ad
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-05-04 13:36:11 +0200

    add notification

---
+++ Assets/Notification.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notification : MonoBehaviour
+{
+    private float distance = 20.0f;
+    private GameObject cameraRef;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      cameraRef =  GameObject.FindWithTag("MainCamera");
+      // find the parent target
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Vector2 cameraPosition = cameraRef.transform.position;
+      Debug.Log(cameraPosition);
+      if(Vector2.Distance(target.position, cameraPosition) < distance) {
+        gameObject.SetActive(true);
+      } else {
+        gameObject.SetActive(false);
+      }
+    }
+}
